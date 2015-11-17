828ee27d141c879b3cfbf0e09e84e4ae26c1ee3a
Andrew Ratliff <andrew.ratliff@gmail.com>
2015-11-17 15:11:30 -0500

    added secret keys

--- app/models/plugins/weather.rb
+++ app/models/plugins/weather.rb
@@ -5,6 +5,11 @@ module Plugins
     end
 
     def handle _
+      darksky = Darksky::API.new('this-is-your-dark-sky-api-key')
+      lat = 38.905922
+      long = -77.041931
     end
+
+      
   end
 end
